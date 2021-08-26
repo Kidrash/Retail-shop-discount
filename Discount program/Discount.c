f6b49356dce6a13b7bd7cb1cdb1d33c81c6c7f23
Kidrash <60612233+Kidrash@users.noreply.github.com>
2021-08-26 09:02:33 +0300

    Update Discount.c

--- Discount program/Discount.c
+++ Discount program/Discount.c
@@ -10,34 +10,36 @@ int main()
     printf("What is your total purchase amount?\n");
     scanf("%d", &purchaseAmount);
 
-    if(purchaseAmount>=10000)
+    if(purchaseAmount >= 10000)
     {
-        discountAmount = (10*purchaseAmount)/100;
-        discountedPrice = (purchaseAmount-discountAmount);
-        printf("Your discount is %d\n",discountAmount);
+        discountAmount = (10 * purchaseAmount) / 100;
+        discountedPrice = (purchaseAmount - discountAmount);
+        printf("Your discount is %d\n", discountAmount);
          printf("your Total is %d\n", discountedPrice);
          return(0);
     }
 
-    else(5000<=purchaseAmount<9999);
+     else if(5000 <= purchaseAmount < 9999);
     {
-        discountAmount = (5*purchaseAmount)/100;
-        discountedPrice = (purchaseAmount-discountAmount);
-          printf("Your discount is %d\n",discountAmount);
-          printf("Your Total is %d\n",discountedPrice);
+        discountAmount = (5 * purchaseAmount) / 100;
+        discountedPrice = (purchaseAmount - discountAmount);
+          printf("Your discount is %d\n", discountAmount);
+          printf("Your Total is %d\n", discountedPrice);
           return(0);
     }
+    //  *****from here the code does not see to work with 3% and 0%***** //
 
-     if(3000<=purchaseAmount<=4999)
+     if(3000 <= purchaseAmount <= 4999)
      {
-         discountAmount = (3*purchaseAmount)/100;
-         discountedPrice = (purchaseAmount-discountAmount);
-           printf("Your discount is %d\n",discountAmount);
-          printf("Your Total is %d\n",discountedPrice);
+         discountAmount = (3  * purchaseAmount) / 100;
+         discountedPrice = (purchaseAmount - discountAmount);
+           printf("Your discount is %d\n", discountAmount);
+          printf("Your Total is %d\n", discountedPrice);
           return(0);
      }
-    if(purchaseAmount<3000)
+     else if(purchaseAmount < 3000)
           printf("Your discount is 0");
-          printf("Your Total is %d\n",purchaseAmount);
+          printf("Your Total is %d\n", purchaseAmount);
      return(0);
 }
+
