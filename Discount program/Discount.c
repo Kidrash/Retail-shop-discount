71ce05c17670aebd77e5befe9fbc4d305d997ba2
Kidrash <60612233+Kidrash@users.noreply.github.com>
2021-08-26 08:36:36 +0300

    Add files via upload
    has some bugs that I am still fixing.

---
+++ Discount program/Discount.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int purchaseAmount;
+    int discountAmount;
+    int discountedPrice;
+
+    printf("What is your total purchase amount?\n");
+    scanf("%d", &purchaseAmount);
+
+    if(purchaseAmount>=10000)
+    {
+        discountAmount = (10*purchaseAmount)/100;
+        discountedPrice = (purchaseAmount-discountAmount);
+        printf("Your discount is %d\n",discountAmount);
+         printf("your Total is %d\n", discountedPrice);
+         return(0);
+    }
+
+    else(5000<=purchaseAmount<9999);
+    {
+        discountAmount = (5*purchaseAmount)/100;
+        discountedPrice = (purchaseAmount-discountAmount);
+          printf("Your discount is %d\n",discountAmount);
+          printf("Your Total is %d\n",discountedPrice);
+          return(0);
+    }
+
+     if(3000<=purchaseAmount<=4999)
+     {
+         discountAmount = (3*purchaseAmount)/100;
+         discountedPrice = (purchaseAmount-discountAmount);
+           printf("Your discount is %d\n",discountAmount);
+          printf("Your Total is %d\n",discountedPrice);
+          return(0);
+     }
+    if(purchaseAmount<3000)
+          printf("Your discount is 0");
+          printf("Your Total is %d\n",purchaseAmount);
+     return(0);
+}
